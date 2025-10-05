64043a884853577f2be7c6f8723975784ad57a8b
crdx <pub@crdx.org>
2025-10-05 22:02:48 +0100

    Upgrade to fiber v3

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package session
 
 import (
+	"context"
 	"time"
 
-	"github.com/gofiber/fiber/v2/middleware/session"
+	"github.com/gofiber/fiber/v3/extractors"
+	"github.com/gofiber/fiber/v3/middleware/session"
 	"github.com/gofiber/storage/mysql"
 )
 
@@ -12,19 +14,41 @@ var s *session.Store
 type Config struct {
 	Table        string        // The table to store the session data in.
 	CookieSecure bool          // Whether the cookie should be HTTPS-only.
-	Expiration   time.Duration // How long the session cookie should last.
+	IdleTimeout  time.Duration // How long the session cookie should last.
+}
+
+type wrapper struct {
+	*mysql.Storage
+}
+
+func (self *wrapper) GetWithContext(_ context.Context, key string) ([]byte, error) {
+	return self.Get(key)
+}
+
+func (self *wrapper) SetWithContext(_ context.Context, key string, value []byte, expiration time.Duration) error {
+	return self.Set(key, value, expiration)
+}
+
+func (self *wrapper) DeleteWithContext(_ context.Context, key string) error {
+	return self.Delete(key)
+}
+
+func (self *wrapper) ResetWithContext(_ context.Context) error {
+	return self.Reset()
 }
 
 // Init initialises the session.
 func Init(sessionConfig *Config, dsn string) {
-	s = session.New(session.Config{
-		Storage: mysql.New(mysql.Config{
-			ConnectionURI: dsn,
-			Table:         sessionConfig.Table,
-		}),
-		KeyLookup:      "cookie:session",
+	s = session.NewStore(session.Config{
+		Storage: &wrapper{
+			Storage: mysql.New(mysql.Config{
+				ConnectionURI: dsn,
+				Table:         sessionConfig.Table,
+			}),
+		},
+		Extractor:      extractors.FromCookie("session"),
 		CookieSecure:   sessionConfig.CookieSecure,
 		CookieHTTPOnly: true,
-		Expiration:     sessionConfig.Expiration,
+		IdleTimeout:    sessionConfig.IdleTimeout,
 	})
 }
